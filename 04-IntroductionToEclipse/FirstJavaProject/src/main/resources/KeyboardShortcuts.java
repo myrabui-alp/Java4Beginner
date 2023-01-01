3cd95da46fc4e3eb274a5d9e118898ceca9ef3b9
Myra Bui ALP (TM Master) <63907332+myrabui-alp@users.noreply.github.com>
2023-01-01 14:40:35 +0700

    First

---
+++ 04-IntroductionToEclipse/FirstJavaProject/src/main/resources/KeyboardShortcuts.java
@@ -0,0 +1,5 @@
+public class KeyboardShortcuts {
+    public static void main(String[] args){
+
+    }
+}
---
+++ 04-IntroductionToEclipse/FirstJavaProject/src/main/resources/MultiplicationTableRunner.java
@@ -0,0 +1,6 @@
+public class MultiplicationTableRunner{
+    public static void main(String[] args){
+        MultiplicationTable = table = new MultiplicationTable();
+        table.print();
+    }
+}
